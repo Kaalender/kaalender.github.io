6f59f35811d9bb3f123b7b829b0fdf27a8b2955e
Pieter-Jan Drouillon <drouillon@hotmail.com>
2016-10-22 12:49:46 +0200

    quantity validation added.

--- assets/js/orders.js
+++ assets/js/orders.js
@@ -1,8 +1,17 @@
-var app = angular.module('orders', []);
+var app = angular.module('orders', ['ui.validate']);
 
 app.controller('OrderCtrl', function($scope){
-  $scope.currentOrder  = {"name":"","email":"","address":"","quantity":"","contribution":""};
+  $scope.currentOrder  = {};
   $scope.printOnConsole = function(){
     console.log($scope.currentOrder);
+  };
+
+  $scope.validateContribution = function(value){
+    minValue = $scope.currentOrder.quantity * 10;
+    if($scope.currentOrder.address != null){
+      //if shipping out, then add 3 euro
+      minValue = minValue + $scope.currentOrder.quantity*3;
+    }
+    return value >= minValue;
   };
 });
