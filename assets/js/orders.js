3ac513669351b211f7fc68da7071bb0780bb96cd
Pieter-Jan Drouillon <drouillon@hotmail.com>
2016-10-22 13:04:49 +0200

    contribution validation added.

--- assets/js/orders.js
+++ assets/js/orders.js
@@ -1,14 +1,14 @@
 var app = angular.module('orders', ['ui.validate']);
 
 app.controller('OrderCtrl', function($scope){
-  $scope.currentOrder  = {};
+  $scope.currentOrder  = {"quantity":1,"contribution":13};
   $scope.printOnConsole = function(){
     console.log($scope.currentOrder);
   };
 
   $scope.validateContribution = function(value){
     minValue = $scope.currentOrder.quantity * 10;
-    if($scope.currentOrder.address != null){
+    if($scope.currentOrder.address != null || $scope.currentOrder.address != ''){
       //if shipping out, then add 3 euro
       minValue = minValue + $scope.currentOrder.quantity*3;
     }
