69aba496bef1fb82fd08ecb885736c1d74050b3c
Pieter-Jan Drouillon <drouillon@hotmail.com>
2016-10-22 14:30:42 +0200

    added checkbox for shipping. Shipping added to db model.

--- assets/js/orders.js
+++ assets/js/orders.js
@@ -1,14 +1,14 @@
 var app = angular.module('orders', ['ui.validate']);
 
 app.controller('OrderCtrl', function($scope){
-  $scope.currentOrder  = {"quantity":1,"contribution":13};
+  $scope.currentOrder  = {"quantity":1,"contribution":10,"shipping":false};
   $scope.printOnConsole = function(){
     console.log($scope.currentOrder);
   };
 
   $scope.validateContribution = function(value){
     minValue = $scope.currentOrder.quantity * 10;
-    if($scope.currentOrder.address != null || $scope.currentOrder.address != ''){
+    if($scope.currentOrder.shipping){
       //if shipping out, then add 3 euro
       minValue = minValue + $scope.currentOrder.quantity*3;
     }
