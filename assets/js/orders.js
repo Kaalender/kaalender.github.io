7ed23d74bebb243874af55b0c51d0df773e106a5
Pieter-Jan Drouillon <drouillon@hotmail.com>
2016-10-22 11:26:21 +0200

    binding form and controller ok.

--- assets/js/orders.js
+++ assets/js/orders.js
@@ -1,5 +1,8 @@
-var app = angular.module('orders', ['ngResource','ngRoute']);
+var app = angular.module('orders', []);
 
-app.controller('MsgCtrl', function($scope){
-  $scope.currentOrder  = {};
+app.controller('OrderCtrl', function($scope){
+  $scope.currentOrder  = {"name":"","email":"","address":"","quantity":"","contribution":""};
+  $scope.printOnConsole = function(){
+    console.log($scope.currentOrder);
+  };
 });
