6f0063452621940803feebaf2a636e67b80d1cf3
Pieter-Jan Drouillon <drouillon@hotmail.com>
2016-10-22 15:47:29 +0200

    added save to db.

--- assets/js/orders.js
+++ assets/js/orders.js
@@ -1,6 +1,8 @@
 var app = angular.module('orders', ['ui.validate']);
 
 app.controller('OrderCtrl', function($scope){
+  API_KEY = "9e3f03d341393fe5ef9d291c7b56ff59f0529";
+  db = new restdb(API_KEY, {});
   $scope.currentOrder  = {"quantity":1,"contribution":10,"shipping":false};
   $scope.printOnConsole = function(){
     console.log($scope.currentOrder);
@@ -13,5 +15,18 @@ app.controller('OrderCtrl', function($scope){
       minValue = minValue + $scope.currentOrder.quantity*3;
     }
     return value >= minValue;
+  };
+
+  $scope.saveOrder = function(){
+    var order = new db.orders($scope.currentOrder);
+    order.save(function(error,result){
+      if(!error){
+        console.log("Saved order just fine");
+      }
+      else{
+        console.log('error saving');
+        console.log(error);
+      }
+    });
   };
 });
